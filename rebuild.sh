fd5c0bfb923e4e8b3505cda23d5ba8cf9ec7d606
munja <allnbwe@gmail.com>
2022-10-10 12:52:02 +0100

    rename

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,15 @@
+
 #!/usr/bin/env bash
 
-rm -rf dist/*
-cp -r ../datacontroller/client/dist/ dist/
+demoloc=$(pwd)
+
+rm -rf docs
+cp -r ../datacontroller/client/dist docs
+echo 'demo.datacontroller.io' > docs/CNAME
+
+sed -i -e 's/serverurl=""/serverurl="https:\/\/sas.4gl.io:5006"/g' docs/index.html
+rm docs/index.html-e
+cd ../datacontroller/sas/mocks
+sasjs cbd -t dcdemo
+
+cd $demoloc
